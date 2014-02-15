13e7999fcebbcbe358ed504bdbf870a525e6ea6a
Daniel Andres <danyandresh@gmail.com>
2014-02-15 17:40:01 +0000

    Add Playlist view and link it from Folder view

---
+++ C#/Desktop/LightPlayer/LightPlayer/Views/Playlist.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace LightPlayer.Views
+{
+    /// <summary>
+    /// Interaction logic for Playlist.xaml
+    /// </summary>
+    public partial class Playlist :Page
+    {
+        public Playlist()
+        {
+            InitializeComponent();
+        }
+    }
+}
