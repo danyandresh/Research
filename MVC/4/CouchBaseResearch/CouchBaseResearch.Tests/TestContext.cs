45933cea4220c729df1b9b3192db6b9479d68c57
Daniel Andres <danyandresh@gmail.com>
2014-03-05 13:43:28 +0000

    Refactored the BeerBucket test to make initialization per test

---
+++ MVC/4/CouchBaseResearch/CouchBaseResearch.Tests/TestContext.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using Ninject.Extensions.Conventions;
+
+namespace CouchBaseResearch.Tests
+{
+    public abstract class TestContext
+    {
+        protected StandardKernel Kernel { get; private set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Kernel = new StandardKernel();
+            Kernel.Bind(x => x
+                .FromAssembliesInPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                .SelectAllClasses()
+                .BindDefaultInterfaces());
+        }
+    }
+}
--- MVC/4/CouchBaseResearch/CouchBaseResearch.Tests/UnitTestBeerSampleBucket.cs
+++ MVC/4/CouchBaseResearch/CouchBaseResearch.Tests/UnitTestBeerSampleBucket.cs
@@ -6,22 +6,16 @@ using Ninject;
 namespace CouchBaseResearch.Tests
 {
     [TestClass]
-    public class UnitTestBeerSampleBucket
+    public class UnitTestBeerSampleBucket : TestContext
     {
         [TestMethod]
         public void BucketHasCouchbaseClient()
         {
-            using (var kernel = new StandardKernel())
-            {
-                //TODO: Use convention over configuration
-                kernel.Bind<IBeerBucket>().To<BeerBucket>();
+            var beerbucket = Kernel.Get<IBeerBucket>();
 
-                var beerbucket = kernel.Get<IBeerBucket>();
-
-                var couchbaseBucket = beerbucket as ICouchbaseBucket;
-                Assert.IsNotNull(couchbaseBucket);
-                Assert.IsNotNull(couchbaseBucket.CouchbaseClient);
-            }
+            var couchbaseBucket = beerbucket as ICouchbaseBucket;
+            Assert.IsNotNull(couchbaseBucket);
+            Assert.IsNotNull(couchbaseBucket.CouchbaseClient);
         }
     }
 }
