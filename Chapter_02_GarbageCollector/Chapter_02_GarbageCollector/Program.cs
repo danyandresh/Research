7bcc9b974d2b3bf45a08b6a470fd5fae7eeeef83
Daniel Andres <danyandresh@gmail.com>
2015-08-03 19:53:29 +0300

    Large object allocations with static reference

--- Chapter_02_GarbageCollector/Chapter_02_GarbageCollector/Program.cs
+++ Chapter_02_GarbageCollector/Chapter_02_GarbageCollector/Program.cs
@@ -8,8 +8,52 @@ namespace Chapter_02_GarbageCollector
 {
     class Program
     {
+        const int ArraySize = 1000;
+
+        static object[] staticArray = new object[ArraySize];
+
         static void Main(string[] args)
         {
+            object[] localArray = new object[ArraySize];
+
+            var rand = new Random();
+
+            for (int i = 0; i < ArraySize; i++)
+            {
+                staticArray[i] = GetNewObject(rand.Next(0, 4));
+                localArray[i] = GetNewObject(rand.Next(0, 4));
+            }
+
+            Console.WriteLine("Use PerfView to examine the heap now. Press any key to exit...");
+            Console.ReadKey();
+
+            Console.WriteLine(staticArray.Length);
+            Console.WriteLine(localArray.Length);
         }
+
+        private static Base GetNewObject(int type)
+        {
+            Base obj = null;
+            switch (type)
+            {
+                case 0: obj = new A(); break;
+                case 1: obj = new B(); break;
+                case 2: obj = new C(); break;
+                case 3: obj = new D(); break;
+            }
+
+            return obj;
+        }
+
+        class Base
+        {
+            private byte[] memory;
+            protected Base(int size) { this.memory = new byte[size]; }
+        }
+
+        class A : Base { public A() : base(1000) { } }
+        class B : Base { public B() : base(10000) { } }
+        class C : Base { public C() : base(1000000) { } }
+        class D : Base { public D() : base(10000000) { } }
     }
 }
