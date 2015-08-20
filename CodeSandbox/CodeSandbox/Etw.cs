e480ee192194d6d42979024f653aac180c2eee59
Daniel Andres <danyandresh@gmail.com>
2015-08-20 16:03:17 +0300

    ETW: Create custom event listeners

--- CodeSandbox/CodeSandbox/Etw.cs
+++ CodeSandbox/CodeSandbox/Etw.cs
@@ -1,30 +1,64 @@
-﻿using System.Diagnostics.Tracing;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
 
 namespace CodeSandbox
 {
     internal class Etw
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var consoleListener = new ConsoleListener(
+                new[]
+                {
+                    new BaseListener.SourceConfig
+                    {
+                        Name = "EtlDemo",
+                        Level = EventLevel.Informational,
+                        Keywords = Events.Keywords.General | Events.Keywords.PrimeOutput
+                    }
+                });
+
+            var start = (long)Math.Pow(10, 6);
+            var end = start + (long)Math.Pow(10, 4);
+
             Events.Write.ProcessingStart();
+            for (var i = start; i < end; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Events.Write.FoundPrime(i);
+                }
+            }
+
             Events.Write.FoundPrime(3);
             Events.Write.ProcessingFinish();
+
+            consoleListener.Dispose();
+        }
+
+        private static bool IsPrime(long number)
+        {
+            var squareRoot = (long)Math.Floor(Math.Sqrt(number));
+            for (long i = 2; i <= squareRoot; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         [EventSource(Name = "EtlDemo")]
         sealed class Events : EventSource
         {
-            public static readonly Events Write = new Events();
-
-            public class Keywords
-            {
-                public const EventKeywords General = (EventKeywords)1;
-                public const EventKeywords PrimeOutput = (EventKeywords)2;
-            }
-
             internal const int ProcessingStartId = 1;
             internal const int ProcessingFinishId = 2;
             internal const int FoundPrimeId = 3;
+            public static readonly Events Write = new Events();
 
             [Event(ProcessingStartId, Level = EventLevel.Informational, Keywords = Keywords.General)]
             public void ProcessingStart()
@@ -35,7 +69,7 @@ namespace CodeSandbox
                 }
             }
 
-            [Event(ProcessingFinishId, Level = EventLevel.Informational, Keywords = Keywords.PrimeOutput)]
+            [Event(ProcessingFinishId, Level = EventLevel.Informational, Keywords = Keywords.General)]
             public void ProcessingFinish()
             {
                 if (IsEnabled())
@@ -60,6 +94,97 @@ namespace CodeSandbox
                 {
                     WriteEvent(5, message ?? string.Empty);
                 }
+            }
+
+            public class Keywords
+            {
+                public const EventKeywords General = (EventKeywords)1;
+                public const EventKeywords PrimeOutput = (EventKeywords)2;
+            }
+        }
+
+        abstract class BaseListener : EventListener
+        {
+            private readonly List<SourceConfig> configs = new List<SourceConfig>();
+
+            protected BaseListener(IEnumerable<SourceConfig> sources)
+            {
+                configs.AddRange(sources);
+
+                foreach (var sourceConfig in configs)
+                {
+                    var eventSource = FindEventSource(sourceConfig.Name);
+
+                    if (eventSource == null)
+                    {
+                        continue;
+                    }
+
+                    EnableEvents(eventSource, sourceConfig.Level, sourceConfig.Keywords);
+                }
+            }
+
+            protected override void OnEventSourceCreated(EventSource eventSource)
+            {
+                base.OnEventSourceCreated(eventSource);
+
+                foreach (
+                    var sourceConfig in
+                        configs.Where(sourceConfig => string.Equals(sourceConfig.Name, eventSource.Name)))
+                {
+                    EnableEvents(eventSource, sourceConfig.Level, sourceConfig.Keywords);
+                }
+            }
+
+            protected override void OnEventWritten(EventWrittenEventArgs eventData)
+            {
+                WriteEvent(eventData);
+            }
+
+            protected abstract void WriteEvent(EventWrittenEventArgs eventData);
+
+            private static EventSource FindEventSource(string name)
+            {
+                var result = EventSource.GetSources()
+                    .FirstOrDefault(eventSource => string.Equals(eventSource.Name, name));
+
+                return result;
+            }
+
+            public class SourceConfig
+            {
+                public string Name { get; set; }
+                public EventLevel Level { get; set; }
+                public EventKeywords Keywords { get; set; }
+            }
+        }
+
+        class ConsoleListener : BaseListener
+        {
+            private readonly IReadOnlyDictionary<int, Func<EventWrittenEventArgs, string>> eventWriteFormaters = new Dictionary
+                <int, Func<EventWrittenEventArgs, string>>
+            {
+                {Events.ProcessingStartId, e => string.Format("ProcessingStart ({0})", e.EventId)},
+                {Events.ProcessingFinishId, e => string.Format("ProcessingFinish ({0})", e.EventId)},
+                {Events.FoundPrimeId, e => string.Format("FoundPrime ({0}):{1}", e.EventId, e.Payload[0])}
+            };
+
+            public ConsoleListener(IEnumerable<SourceConfig> sources)
+                : base(sources)
+            {
+            }
+
+            protected override void WriteEvent(EventWrittenEventArgs eventData)
+            {
+                Func<EventWrittenEventArgs, string> outputStringFormater;
+                if (!eventWriteFormaters.TryGetValue(eventData.EventId, out outputStringFormater))
+                {
+                    throw new InvalidOperationException("Unknown event: " + eventData.EventId);
+                }
+
+                var outputMessage = outputStringFormater(eventData);
+
+                Console.WriteLine(outputMessage);
             }
         }
     }
