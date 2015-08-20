718d26af90c541e004f0f40576dc7842a86ff2f5
Daniel Andres <danyandresh@gmail.com>
2015-08-20 11:00:53 +0300

    Create and consume custom performance counters

---
+++ CodeSandbox/CodeSandbox/PerformanceCounters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CodeSandbox
+{
+    internal class PerformanceCounters
+    {
+        private static void Main(string[] args)
+        {
+            var counterDataCollection = new CounterCreationDataCollection();
+
+            var bytesPerTx = new CounterCreationData
+            {
+                CounterType = PerformanceCounterType.AverageCount64,
+                CounterName = "BytesPerTransmission"
+            };
+
+            counterDataCollection.Add(bytesPerTx);
+
+            var bytesPerTxBase = new CounterCreationData
+            {
+                CounterType = PerformanceCounterType.AverageBase,
+                CounterName = "BytesPerTransmissionBase"
+            };
+
+            counterDataCollection.Add(bytesPerTxBase);
+
+            if (!PerformanceCounterCategory.Exists("Network Statistics"))
+            {
+                PerformanceCounterCategory.Create(
+                    "Network Statistics",
+                    "Network statistics demo counters",
+                    PerformanceCounterCategoryType.SingleInstance,
+                    counterDataCollection);
+            }
+
+            var networkStatsCounter = new PerformanceCounter("Network Statistics", "BytesPerTransmission", false);
+            networkStatsCounter.IncrementBy(1000);
+            var counterValue = networkStatsCounter.RawValue;
+
+            Console.WriteLine(counterValue);
+        }
+    }
+}
