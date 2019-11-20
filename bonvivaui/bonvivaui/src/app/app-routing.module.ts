cde48c7b1c8c141028201ad74597068910d087f0
Daniel <daniel.n.andres@gmail.com>
2019-11-20 23:03:44 +0000

    UI: Implements link to network size

--- bonvivaui/bonvivaui/src/app/app-routing.module.ts
+++ bonvivaui/bonvivaui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './login/login.component'
+import { LoginComponent } from './login/login.component'
+import { NetworkSizeComponent } from './network-size/network-size.component'
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'ns', component: NetworkSizeComponent },
 ];
 
 @NgModule({
--- bonvivaui/bonvivaui/src/app/usersession.service.ts
+++ bonvivaui/bonvivaui/src/app/usersession.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersessionService {
 
-  constructor() { }
-  
-  login() {
-    return console.log('clicked');
+  constructor(private router: Router) { }
+
+  _username: String
+
+  login(username: String) {
+    this._username = username;
+    console.log(username);
+
+    this.router.navigate(['/ns']);
   }
 }
